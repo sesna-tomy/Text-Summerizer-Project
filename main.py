1d6f2e2b28f83a5c7e317719eae06a43c7ff8f4b
sesna-tomy <sesnatomy1999@gmail.com>
2024-03-31 12:39:02 +0530

    Data Validation done

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from scr.textSummarizer.pipeline.stage01_data_ing import DataIngestionTrainingPipeline
 from scr.textSummarizer.logging import logger
+from scr.textSummarizer.pipeline.stage02_data_val import DataValidationTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion Stage"
@@ -8,6 +9,19 @@ try:
     logger.info(f">>>> stage {STAGE_NAME} started <<<<<")
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
+    logger.info(f"<<<<< stage {STAGE_NAME} completed >>>>>\n\nX====X")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+
+STAGE_NAME = "Data Validation Stage"
+
+try:
+    logger.info(f">>>> stage {STAGE_NAME} started <<<<<")
+    data_validation = DataValidationTrainingPipeline()
+    data_validation.main()
     logger.info(f"<<<<< stage {STAGE_NAME} completed >>>>>\n\nX====X")
 except Exception as e:
     logger.exception(e)
--- scr/textSummarizer/config/configuration.py
+++ scr/textSummarizer/config/configuration.py
@@ -1,6 +1,6 @@
 from scr.textSummarizer.constants import *
 from scr.textSummarizer.utils.common import read_yaml, create_directories
-from scr.textSummarizer.entity import DataIngestionConfig
+from scr.textSummarizer.entity import DataIngestionConfig, DataValidationConfig
 
 
 
@@ -28,3 +28,16 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+    
+    def get_data_val(self) -> DataValidationConfig:
+            config = self.config.data_validation
+
+            create_directories([config.root_dir])
+
+            data_validation_config = DataValidationConfig(
+                root_dir = config.root_dir,
+                STATUS_FILE= config.STATUS_FILE,
+                ALL_REQUIRED_FILES= config.ALL_REQUIRED_FILES            
+            )
+
+            return data_validation_config
